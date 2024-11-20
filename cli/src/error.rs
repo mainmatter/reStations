0c39a0708f4781fbf17d82daab2a7c94d0e70487
Henk Oordt <hd@oordt.dev>
2024-11-20 17:23:58 +0100

    get csv response as AsyncRead

--- cli/src/error.rs
+++ cli/src/error.rs
@@ -2,4 +2,7 @@
 pub enum Error {
     #[error("HTTP error: {0}")]
     HttpClient(#[from] reqwest::Error),
+
+    #[error("I/O error: {0}")]
+    Io(#[from] tokio::io::Error),
 }
--- cli/src/sync.rs
+++ cli/src/sync.rs
@@ -1,5 +1,8 @@
 use clap::Args;
 use reqwest;
+use tokio::io::AsyncReadExt;
+use tokio_stream::StreamExt;
+use tokio_util::io::StreamReader;
 
 use crate::{error::Error, CommonArgs};
 
@@ -11,10 +14,21 @@ pub struct SyncAction {
 impl SyncAction {
     pub async fn exec(self, _common: CommonArgs) -> Result<(), Error> {
         // 1. streamingly fetch csv from https://raw.githubusercontent.com/trainline-eu/stations/refs/heads/master/stations.csv,
-        let body = reqwest::get("https://raw.githubusercontent.com/trainline-eu/stations/refs/heads/master/stations.csv")
+
+        // Get the response bytes as stream (https://docs.rs/futures/latest/futures/prelude/trait.Stream.html)
+        let stream = reqwest::get("https://raw.githubusercontent.com/trainline-eu/stations/refs/heads/master/stations.csv")
             .await?
-            .text()
-            .await?;
+            .bytes_stream()
+            // map items from a Result<Bytes, reqwest::Error> to Result<Bytes, tokio::io::Error>,
+            // in order for the stream to be wrapped in a StreamReader
+            .map(|r| r.map_err(tokio::io::Error::other));
+        // Wrap the stream in a StreamReader, which implements AsyncRead, the trait csv-async is built around
+        let mut reader = StreamReader::new(stream);
+
+        // For now we just read the entire body in a string and print it
+        let mut body = String::new();
+        reader.read_to_string(&mut body).await?;
+
         // 2. pipe the data into https://github.com/gwierzchowski/csv-async, and deserialize to [`stations_core::data::StationRecord`]
 
         // 3. pipe deserialized data into database
