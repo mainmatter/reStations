212f3e66c25292ce40c341a9da2a7792ae11e663
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2024-12-05 20:39:27 -0600

    Use serde_rusqlite to deserialize rows into StationRecord
    Not yet working:
    
    ```
    2024-12-06T02:37:09.513395Z ERROR tracing_panic: A panic occurred panic.payload="called `Result::unwrap()` on an `Err` value: Deserialization { column: Some(\"id\"), message: \"invalid type: Option value, expected a string\" }" panic.location="web/src/controllers/stations.rs:22:34" panic.backtrace=disabled backtrace panic.note="run with RUST_BACKTRACE=1 environment variable to display a backtrace"
    2024-12-06T02:37:10.537839Z ERROR tracing_panic: A panic occurred panic.payload="called `Result::unwrap()` on an `Err` value: PoisonError { .. }" panic.location="web/src/controllers/stations.rs:13:35" panic.backtrace=disabled backtrace panic.note="run with RUST_BACKTRACE=1 environment variable to display a backtrace"
    2024-12-06T02:37:15.554781Z ERROR tracing_panic: A panic occurred panic.payload="called `Result::unwrap()` on an `Err` value: PoisonError { .. }" panic.location="web/src/controllers/stations.rs:13:35" panic.backtrace=disabled backtrace panic.note="run with RUST_BACKTRACE=1 environment variable to display a backtrace"
    ^C
    ```

--- web/src/controllers/stations.rs
+++ web/src/controllers/stations.rs
@@ -1,11 +1,9 @@
 use axum::response::Json;
-use serde::{Deserialize, Serialize};
-use rusqlite::{Connection};
 // TODO figure out why only using types:: doesn't work here
 use crate::types::station_record::StationRecord;
 use crate::state::SharedAppState;
-use std::sync::Arc;
 use axum::extract::State;
+use serde_rusqlite::from_rows;
 
 // TODO what's the right notation here for a collection of station records?
 /// Responds with a [`[StationRecord]`], encoded as JSON.
@@ -15,19 +13,15 @@ pub async fn list(State(app_state): State<SharedAppState>) -> Json<Vec<StationRe
     let locked_conn = conn.lock().unwrap();
 
     let mut stmt = locked_conn.prepare("SELECT * from stations").unwrap();
-    let rows = stmt.query([]).unwrap();
 
+    let res = from_rows::<StationRecord>(stmt.query([]).unwrap());
     let mut stations = Vec::new();
-    for row in rows {
-        // TODO map row to StationRecord
-        // from_row() doesn't exist yet
-        // let station_result = StationRecord::from_row(row?);
-        let station_result = StationRecord {
-            id: "1".to_string(),
-            ..Default::default()
-        };
-        stations.push(station_result);
+
+    for station in res {
+        // stations.push(station.unwrap());
+        println!("{:?}", station.unwrap());
     }
+
 
     Json(stations)
 }
