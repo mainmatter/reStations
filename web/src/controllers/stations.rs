3d6d14e8a66a08baf9e55b292ff2d43b28cdcef4
Pablo Brasero <pablo@pablobm.com>
2025-01-09 11:24:48 +0000

    Remove final reference to rusqlite outside db

--- web/src/controllers/stations.rs
+++ web/src/controllers/stations.rs
@@ -3,7 +3,8 @@ use axum::response::Json;
 use crate::state::SharedAppState;
 use crate::types::station_record::StationRecord;
 use axum::extract::State;
-use serde_rusqlite::from_rows;
+
+use super::super::db;
 
 // TODO what's the right notation here for a collection of station records?
 /// Responds with a [`[StationRecord]`], encoded as JSON.
@@ -12,21 +13,7 @@ pub async fn list(State(app_state): State<SharedAppState>) -> Json<Vec<StationRe
     let conn = app_state.conn.clone();
     let locked_conn = conn.lock().unwrap();
 
-    let mut stmt = locked_conn.prepare("SELECT * from stations").unwrap();
-
-    // let mut rows = stmt.query([]).unwrap();
-
-    // while let Ok(row) = rows.next() {
-    //     println!("{:?}", row.unwrap());
-    // }
-
-    let res = from_rows::<StationRecord>(stmt.query([]).unwrap());
-    let mut stations: Vec<StationRecord> = Vec::new();
-
-    for station in res {
-        stations.push(station.unwrap());
-        // println!("{:?}", station.unwrap());
-    }
+    let stations = db::find_all_stations(&locked_conn);
 
     Json(stations)
 }
