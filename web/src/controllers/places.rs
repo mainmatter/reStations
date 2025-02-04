24c4dd520d425ab68a1be4d9cb7495b4be2c5129
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2025-02-04 15:11:23 +0000

    Minimal Place definition and serialization

--- web/src/controllers/places.rs
+++ web/src/controllers/places.rs
@@ -1,17 +1,56 @@
 use super::super::db;
 use crate::state::SharedAppState;
-use crate::types::station_record::StationRecord;
 use axum::extract::{Path, State};
 use axum::response::Json;
+use serde::Serialize;
+
+#[derive(Serialize)]
+pub struct Place {
+    id: i64,
+    object_type: String,
+    alternative_ids: Vec<String>,
+    geo_position: GeoPosition,
+    _links: Vec<ApiLink>,
+}
+#[derive(Serialize)]
+pub struct GeoPosition {
+    latitude: f64,
+    longitude: f64,
+}
+#[derive(Serialize)]
+pub struct ApiLink {
+    rel: String,
+    href: String,
+    _type: String,
+    value: String,
+}
 
 #[axum::debug_handler]
-pub async fn show(
-    State(app_state): State<SharedAppState>,
-    Path(id): Path<u64>,
-) -> Json<StationRecord> {
+pub async fn show(State(app_state): State<SharedAppState>, Path(id): Path<u64>) -> Json<Place> {
     let conn = app_state.pool.get().unwrap();
 
     let station = db::find_station(&conn, id).unwrap();
+    let latitude = station
+        .latitude
+        .parse::<f64>()
+        .expect("Failed to parse latitude");
+    let longitude = station
+        .longitude
+        .parse::<f64>()
+        .expect("Failed to parse longitude");
 
-    Json(station)
+    let geo_position = GeoPosition {
+        latitude: latitude,
+        longitude: longitude,
+    };
+
+    let place = Place {
+        id: station.id,
+        object_type: "StopPlace".into(),
+        alternative_ids: vec![],
+        geo_position,
+        _links: vec![],
+    };
+
+    Json(place)
 }
