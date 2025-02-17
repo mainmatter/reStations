ae74a18c250707033276bfd3d04a609b3daf65f3
Oliver Azevedo Barnes <oliver@liquidvoting.io>
2025-02-17 19:27:29 +0000

    rust fmt

--- web/tests/api/stations_test.rs
+++ web/tests/api/stations_test.rs
@@ -1,9 +1,9 @@
-use googletest::prelude::{assert_that,eq};
+use googletest::prelude::{assert_that, eq};
 use restations_macros::test;
 use restations_web::test_helpers::{BodyExt, RouterExt, TestContext};
 
-use restations_web::db;
 use restations_web::controllers::stations::StationsListResponse;
+use restations_web::db;
 
 use restations_web::types::station_record::StationRecord;
 
@@ -18,19 +18,20 @@ async fn test_list_empty(context: &TestContext) {
 #[test]
 async fn test_list_single_record(context: &TestContext) {
     let dbconn = context.pool.get().unwrap();
-    let _ = db::create_tables(&dbconn)
-        .expect("Could not create DB tables");
+    let _ = db::create_tables(&dbconn).expect("Could not create DB tables");
     let test_station = StationRecord {
         id: 1,
         name: String::from("Test Station"),
         slug: String::from("test-station"),
         ..Default::default()
     };
-    let _ = db::insert_station(&dbconn, &test_station)
-        .expect("Could not insert station in DB");
+    let _ = db::insert_station(&dbconn, &test_station).expect("Could not insert station in DB");
 
     let response = context.app.request("/stations").send().await;
-    let stations: StationsListResponse = response.into_body().into_json::<StationsListResponse>().await;
+    let stations: StationsListResponse = response
+        .into_body()
+        .into_json::<StationsListResponse>()
+        .await;
 
     assert_that!(stations.len(), eq(1));
     let station = &stations[0];
