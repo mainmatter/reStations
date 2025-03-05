7c12ebf74bb1d0b76c809f291c00378c6b15dfac
Marco Otte-Witte <marco.otte-witte@mainmatter.com>
2025-03-05 09:49:42 +0100

    remove Heroku stuff

--- scripts/start.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-# Heroku requires us to re-bind some variables provided only at boot time for the server to run.
-
-export APP_SERVER__PORT="$PORT"
-export APP_SERVER__IP="0.0.0.0"
-
-/usr/local/bin/restations-web
